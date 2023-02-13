8655e5f7a1593057da470b0878579fe603ad08bf
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-13 18:35:02 +0100

    modifies route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ComicsController as ComicsController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ComicsController::class, 'index'])->name('index');

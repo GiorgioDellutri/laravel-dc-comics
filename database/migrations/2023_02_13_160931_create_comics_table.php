10f40772b4ffe78ef06e38c6f720cd778f652352
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-13 18:37:11 +0100

    created columns

--- database/migrations/2023_02_13_160931_create_comics_table.php
+++ database/migrations/2023_02_13_160931_create_comics_table.php
@@ -11,10 +11,19 @@ return new class extends Migration
      *
      * @return void
      */
+
+
     public function up()
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('thumb',);
+            $table->string('title', 255);
+            $table->text('description');
+            $table->string('series', 255);
+            $table->float('price');
+            $table->date('sale_date');
+            $table->string('type', 100);
             $table->timestamps();
         });
     }

c973136b5f3d7070b3a473bb31a221e8ae21754f
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-13 18:55:11 +0100

    create table with foreach

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,7 +1,30 @@
 @extends('layouts.app')
 
 @section('main-content')
-    @foreach ($comics as $comic)
-        <p>{{ $comic->title }}</p>
-    @endforeach
+    <div class="container-fluid">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Series</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Sale date</th>
+                    <th scope="col">Price</th>
+                </tr>
+            </thead>
+            @foreach ($comics as $comic)
+                <tbody>
+                    <tr>
+                        <th scope="row">{{ $comic->id }}</th>
+                        <td>{{ $comic->title }}</td>
+                        <td>{{ $comic->series }}</td>
+                        <td>{{ $comic->type }}</td>
+                        <td>{{ $comic->sale_date }}</td>
+                        <td>{{ $comic->price }}</td>
+                    </tr>
+                </tbody>
+            @endforeach
+        </table>
+    </div>
 @endsection

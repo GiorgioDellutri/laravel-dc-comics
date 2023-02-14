2c3441e6a8b0ddd3354d48f97bf99c5e8a834a25
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-14 16:02:56 +0100

    created form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,9 +1,41 @@
 @extends('layouts.app')
 
-{{-- @section('title, "Create new comic") --}}
-
 @section('main-content')
     <div class="container-fluid">
-
+        <div class="row">
+            <div class="col-4 m-auto">
+                <form>
+                    <div class="mb-3">
+                        <label class="form-label">Thumb</label>
+                        <input type="text" class="form-control" id="title" name="title">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Title</label>
+                        <input type="text" class="form-control" id="title" name="title">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label d-block">Description</label>
+                        <textarea name="description" id="" cols="80" rows="5"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Price</label>
+                        <input type="text" class="form-control" id="title" name="title">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Series</label>
+                        <input type="text" class="form-control" id="title" name="title">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Type</label>
+                        <input type="text" class="form-control" id="title" name="title">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Comic name</label>
+                        <input type="text" class="form-control" id="title" name="title">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
     </div>
 @endsection

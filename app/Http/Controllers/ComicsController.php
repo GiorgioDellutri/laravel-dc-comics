24f1124d4d1aaecaa3f3857c6c66ce5287f131c1
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-14 17:10:15 +0100

    create data in store method

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -39,6 +39,17 @@ class ComicsController extends Controller
     public function store(Request $request)
     {
         //
+        $data = $request->all();
+        $newComic = new Comic();
+        $newComic->thumb = $data['thumb'];
+        $newComic->title = $data['title'];
+        $newComic->description = $data['description'];
+        $newComic->price = $data['price'];
+        $newComic->series = $data['series'];
+        $newComic->sale_date = $data['sale_date'];
+        $newComic->type = $data['type'];
+        $newComic->save();
+        return redirect()->route('comics.show', $newComic->id);
     }
 
     /**

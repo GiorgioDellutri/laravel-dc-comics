ef71457f32219700f2c4a1fb787f1c98d4898afc
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-14 14:31:08 +0100

    added image classes

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,12 +5,11 @@
         <div class="row d-flex justify-content-center">
             <div class="col-12 d-flex justify-content-center">
                 <div class="card" style="width: 20rem;">
-                    <img src="{{ $comic->thumb }}" class="card-img-top" alt="comic img">
+                    <img src="{{ $comic->thumb }}" class="card-img-top img-fluid" alt="comic img">
                     <div class="card-body ">
                         <h3 class="text-center">Title: {{ $comic->title }}</h3>
-                        <p>Description: {{ $comic->description }}</p>
                         <p>Series: {{ $comic->series }}</p>
-                        <p>Price: {{ $comic->price }} $</p>
+                        <p>Description: {{ $comic->description }}</p>
                     </div>
                 </div>
             </div>

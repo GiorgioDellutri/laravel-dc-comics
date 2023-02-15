4eb1cf11782002999d5a361318f1d0ba5f8ee5f9
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-15 16:17:06 +0100

    added edit button

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -16,6 +16,7 @@
                     <th scope="col">Type</th>
                     <th scope="col">Sale date</th>
                     <th scope="col">Price</th>
+                    <th scope="col">Utilities</th>
                 </tr>
             </thead>
             @foreach ($comics as $comic)
@@ -27,7 +28,11 @@
                         <td>{{ $comic->type }}</td>
                         <td>{{ $comic->sale_date }}</td>
                         <td>{{ $comic->price }}</td>
-                        <td><a href="{{ route('comics.show', $comic->id) }}" class="btn btn-sm btn-success">Show</a></td>
+                        <td>
+                            <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-sm btn-success m-2">Show</a>
+                            <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-sm btn-dark m-2">Edit</a>
+                        </td>
+                        </td>
                     </tr>
                 </tbody>
             @endforeach

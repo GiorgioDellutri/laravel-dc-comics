7d43bff4c99c1ac5b3cf070080a3388d8342db3c
GiorgioDellutri <giorgiodll93@gmail.com>
2023-02-16 17:33:46 +0100

    created navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,8 @@
+<nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand fs-2" href="{{ route('comics.index') }}">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/3/3d/DC_Comics_logo.svg" alt="Logo" width="30"
+                height="24" class="d-inline-block align-text-top">
+            Dc Comics
+        </a>
+</nav>
